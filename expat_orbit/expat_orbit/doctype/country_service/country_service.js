796e4e5480be5dab072d1d092d776e4b9fb0604e
abrar.pv <abrar.pv@nestorbird.com>
2022-09-18 21:20:28 +0530

    Expat orbit initial commit

---
+++ expat_orbit/expat_orbit/doctype/country_service/country_service.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, abrarpv97@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Country Service', {
+	// refresh: function(frm) {
+
+	// }
+});
